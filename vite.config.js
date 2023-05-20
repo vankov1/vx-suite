ecc6efe1dab01c24ff4a3b94ed7f93840998270e
Val Vankov <val@vankov.me>
2023-05-21 02:13:59 +0300

    Update

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
   build: {
     lib: {
       // Could also be a dictionary or array of multiple entry points
-      entry: resolve(__dirname, 'src/index.js'),
+      entry: resolve(__dirname, 'lib/index.js'),
       formats: ['es', 'cjs'],
       // the proper extensions will be added
       fileName: (format) => `vx-suite.${format}.js`,
@@ -20,9 +20,10 @@ export default defineConfig({
       output: {
         // Provide global variables to use in the UMD build
         // for externalized deps
-        inlineDynamicImports: false,
-        preserveModules: true,
+        // inlineDynamicImports: false,
+        // preserveModules: true,
         exports: 'named',
+        sourcemap: true,
         globals: {
           vue: 'Vue',
         },
