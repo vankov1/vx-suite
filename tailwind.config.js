1667d577335eaa9ac76c56529a95f5e1ec35f112
Val Vankov <val@vankov.me>
2023-05-20 07:07:04 +0300

    Update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,8 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [],
+export default {
+  content: [
+    './src/**/*.{html,js,.vue}'
+  ],
   theme: {
     extend: {},
   },
